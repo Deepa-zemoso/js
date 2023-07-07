413ba82e49a433ca6846b298ba3aa533c676e38c
Deepa-zemoso <138880495+Deepa-zemoso@users.noreply.github.com>
2023-07-07 18:38:59 +0530

    Assignment 1

---
+++ helloworld.js
@@ -0,0 +1,59 @@
+
+// Assignment 1: 
+//Question 1: passing function as a parameter to another function
+
+function first(i, j) {
+    let sum = i + j;
+    return sum;
+}
+
+function second(func) {
+    console.log("Sum of 2 numbers : " + func(1,2));
+}
+
+// pass first function as a parameter to second function
+second(first);
+
+//Question 2: arrow function
+
+let getFirstCharFunc = (a,b) => {
+    return a.charAt(0) + b.charAt(0);
+};
+
+console.log(getFirstCharFunc("Good", "Morning"));
+
+//Question: Refactor
+// const printName = (name) => {
+//     return “Hi” + name;
+// }
+const printName = (name) => {return "Hi " + name};
+console.log(printName("xyz"));
+
+//Question: Refactor
+// const printBill = (name, bill) => {
+//     return “Hi “ + name + “, please pay: “ + bill;
+// }
+const printBill = (name, bill) => {
+    return `Hi  + ${name} + , please pay:  + ${bill}`;
+}
+console.log(printBill("xyz",100))
+
+//Question:
+const person = {
+    name:"Noam Chomsky",
+    age:92
+}
+const {name: personName, age: personAge} = person;
+
+console.log("name " + personName + " and age " + personAge);
+
+function loadScript(src) {
+    let script = document.createElement('script');
+    script.src = src; 
+    document.head.append(script);
+  }
+  
+  loadScript('/src/helloworld.js');
+
+
+
